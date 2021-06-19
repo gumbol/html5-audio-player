07fd77f85500f4b42cc1d70ca1ae5c90dfdc635d
gumbol <gumbol@github.io>
2021-06-19 06:22:48 +0300

    a lightweight version

---
+++ README.md
@@ -0,0 +1,37 @@
+# html5-audio-player
+ <img src="https://img.shields.io/badge/License-MIT-blue.svg">
+  
+A custom HTML5 audio player app that include features like 
+
+- **Custom control buttons**
+- **Custom volume slider**
+- **Custom display for track duration**
+- **Custom display for current track time**
+
+[Here is a Demo](https://html5-audio-player.herokuapp.com)
+
+## Requirments
+jquery v3.3.1 or heigher (this is included with this repo)
+
+
+## Getting started
+* clone the repository
+```html
+$git clone https://github.com/gumbol/html5-audio-player
+```
+
+> If you did not download or clone the repository then make sure that you have the files in the same directory structure locally. Otherwise you will need to point public/index.html to the right file path.
+
+## Note
+This is a simple demo of the flexability
+and the power of the new html audio object. 
+future development on this app will include
+
+* Process json and xml data
+* Automate playlist creation
+* Make seek bar draggable
+* Adjust volume slider width to a more convenient length
+* Add numeral scale to the volume control
+
+## Licenses
+This app is under MIT license
---
+++ scripts/html5-audio-player-v1.js
@@ -0,0 +1,172 @@
+$(function () {
+
+	var audio;
+
+
+
+	//Hide Pause
+	$('#pause').hide();
+
+	//Initialize the audio player with the first soundtrack
+	initAudio($('#playlist li:first-child'));
+
+	function initAudio(element) {
+		var song = element.attr('song');
+		var cover = element.attr('cover');
+		var temp = parseFloat($('#volume-slider').val() / 100);
+
+
+		//Create audio object
+		audio = new Audio('https://archive.org/download/KayrayReadsToYou2/' + song);
+
+
+		audio.volume = temp;
+
+		audio.addEventListener("loadedmetadata", function () {
+			minutes = parseInt(audio.duration / 60);
+			seconds = parseInt(audio.duration % 60);
+
+			if (seconds < 10) {
+				seconds = '0' + seconds;
+			};
+			if (minutes < 10) {
+				minutes = '0' + minutes;
+			};
+
+			$('#duration').html(minutes + ':' + seconds);
+
+		});
+
+		audio.addEventListener('ended', function () {
+			if (count > 0) {
+				var next = $('#playlist li.active').next();
+				if (next.length == 0) {
+					next = $('#playlist li:first-child');
+				}
+				initAudio($(next));
+				audio.play();
+				showCurrentTime();
+			}
+		});
+
+
+
+		$('#duration').text();
+		//Insert song cover. 
+		//NOTE: the src path below is relative to the 
+		//html file location not this js file location.
+		$('img.cover').attr('src', '../media/hobbit/' + cover);
+		$('#playlist li').removeClass('active');
+		element.addClass('active');
+	};
+
+
+	//@Main buttons code
+
+	$('#play').click(function () {
+		audio.play();
+	
+		$("#play").hide();
+		$("#pause").show();
+
+		showCurrentTime();
+		//audio.volume = parseFloat(this.value);
+	});
+
+	$('#pause').click(function () {
+		audio.pause();
+		$("#pause").hide();
+		$("#play").show();
+	});
+
+
+	$('#next').click(function () {
+		audio.pause();
+		var next = $('#playlist li.active').next();
+
+		if (next.length == 0) {
+			next = $('#playlist li:first-child');
+		}
+		
+		initAudio(next);
+		audio.play();
+		showCurrentTime();
+	});
+
+	$('#prev').click(function () {
+		audio.pause();
+		var prev = $('#playlist li.active').prev();
+		if (prev.length == 0) {
+			prev = $('#playlist li:last-child');
+		}
+	
+		initAudio(prev);
+		audio.play();
+		showCurrentTime();
+	
+	});
+
+	//@end
+
+	//@Playlist song click
+	$('#playlist li').click(function () {
+
+		audio.pause();
+		initAudio($(this));
+		$('#play').hide();
+		$('#pause').show();
+		audio.play();
+		showCurrentTime();
+	});
+
+	//@end
+
+
+	//@Volume control
+
+	$('#volume-slider').change(function () {
+		audio.volume = parseFloat($('#volume-slider').val() / 100);
+	});
+
+	//@end
+
+
+
+
+	//@Show the track's current time in minutes and seconds
+	//and animate the progress bar
+
+	function showCurrentTime() {
+
+		$(audio).bind('timeupdate', function () {
+
+
+			var minutes = parseInt(audio.currentTime / 60) % 60;
+			var seconds = parseInt(audio.currentTime % 60);
+
+			if (minutes < 10) {
+				var minutes = '0' + minutes;
+			}
+
+			if (seconds < 10) {
+				var seconds = '0' + seconds;
+			}
+
+
+			$('#current-time').html(minutes + ':' + seconds);
+
+			var value = 0;
+
+			if (audio.currentTime > 0) {
+				value = Math.floor((100 / audio.duration) * audio.currentTime);
+			}
+			$('#seeker').css('width', value + '%');
+		});
+
+	}
+
+	//@end
+});
+
+
+
